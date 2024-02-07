4f3ae6ef077ec51ae34c836acf5d95c85286003a
Yusifovvv <157576639+Yusifovvv@users.noreply.github.com>
2024-02-07 11:24:01 +0300

    Задание 4.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#Задание5
-N,A,B,C=map(int,input("N A B C:").split())
-print(A*100/N,"% пятерок")
-print(B*100/N,"% четверок")
-print(C*100/N,"% троек")
+#Номер 4
+f = float(input())
+print(5 * (f - 32) / 9)
