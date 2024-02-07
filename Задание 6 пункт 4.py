889a787d7927d70ae7bb40fb5e2190554fe5cbeb
Yusifovvv <157576639+Yusifovvv@users.noreply.github.com>
2024-02-07 14:04:49 +0300

    Create Задание 6 пункт 4.py

---
+++ Задание 6 пункт 4.py
@@ -0,0 +1,7 @@
+# Задание6 пункт 4 
+import math
+from math import cos,sin, pow
+x3 = int(input("x3:"))
+y = int(input("y:"))
+D = math.pow(math.pow(math.sin(x3),2)+math.pow(math.cos(math.pow(y, 3)), 2),0.5)
+print(D)
